b6936e8832c042bc711320654a23a5bce3f289da
Johan Schultze <Johan.schultze@gmail.com>
2019-02-12 14:55:24 +0100

    Eric Johan

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -8,40 +8,17 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 
 public class Controller {
 
-    TextField input;
-    TextArea messages;
-    String message;
+    public Button sendBtn;
 
-    private Parent createContent() {
-
-        TextField input = new TextField();
-//        Display on screen if message comes from client or server
-        input.setOnAction(event -> {
-            message += input.getText();
-            //Clear text from box after enter
-            input.clear();
-
-            messages.appendText(message + "\n");
-
-            try {
-                connection.send(message);
-                NetworkConnection.s
-
-            } catch (Exception e) {
-                messages.appendText("Failed to send.\n");
-
-            }
-        });
-
-        VBox root = new VBox(20, messages, input);
-//        root.setPrefSize(600, 600);
-        return root;
+    public void sendBtnClick(){
+        sendBtn.setText("Hello");
     }
-}
+
+
+
+    }
