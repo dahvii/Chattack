eb05fb4604c7c440e65e94fc9060491431acb0f1
Johan Schultze <Johan.schultze@gmail.com>
2019-02-12 13:27:28 +0100

    funkar inte

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,8 +1,47 @@
-package ChatApp;
+package gui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+
+import ChatApp.ChatApp;
+import ChatApp.NetworkConnection;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Controller {
 
+    TextField input;
+    TextArea messages;
+    String message;
+
+    private Parent createContent() {
+
+        TextField input = new TextField();
+//        Display on screen if message comes from client or server
+        input.setOnAction(event -> {
+            message += input.getText();
+            //Clear text from box after enter
+            input.clear();
+
+            messages.appendText(message + "\n");
+
+            try {
+                connection.send(message);
+                NetworkConnection.s
+
+            } catch (Exception e) {
+                messages.appendText("Failed to send.\n");
+
+            }
+        });
+
+        VBox root = new VBox(20, messages, input);
+//        root.setPrefSize(600, 600);
+        return root;
+    }
 }
