7a2e9515ed23e07e3976b0566004ba42f4ef835d
Eric Rasmusson Lindström <eric.rl@me.com>
2019-02-12 15:48:57 +0100

    Kan skriva och skicka

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -9,16 +9,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 
 public class Controller {
 
     public Button sendBtn;
+    public TextField input;
 
     public void sendBtnClick(){
-        sendBtn.setText("Hello");
+        String message = input.getText();
+        sendBtn.setText("Send");
+        input.clear();
         ChatApp chatApp = new ChatApp();
-        chatApp.send("HELLOOOOO");
+        chatApp.send(message);
     }
 
 
