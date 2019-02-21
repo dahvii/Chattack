4592993cf393d7844204adbd2eb78f8c063c392d
Eric Rasmusson Lindström <eric.rl@me.com>
2019-02-21 09:26:14 +0100

    Added functionality to Password input

--- src/Client/gui/Controller.java
+++ src/Client/gui/Controller.java
@@ -103,11 +103,13 @@ public class Controller {
         okButton.setOnAction(e -> {
             //om användaren inte har fyllt i ett namn
             //remove whitespaces
+            String password = passwordInput.getText().replaceAll("\\s+", "");
             String name = nameInput.getText().replaceAll("\\s+","");
-            if( name.equals("")){
+            if( name.equals("") & password.equals("")) {
                 errorMessage.setStyle("visibility: visible;");
             } else{ // om användaren  fyllt i ett namn
                 user.setName(name);
+                user.setPassword(password);
                 window.close();
             }
 
